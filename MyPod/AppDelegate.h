37aae75e2ea70342a05b78ac0281cce153248dee
xuzepei <xuzepei@smalltreemedia.com>
2018-07-10 09:54:21 +0800

    test

---
+++ MyPod/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyPod
+//
+//  Created by xuzepei on 2018/7/10.
+//  Copyright © 2018年 xuzepei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyPod/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyPod
+//
+//  Created by xuzepei on 2018/7/10.
+//  Copyright © 2018年 xuzepei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
